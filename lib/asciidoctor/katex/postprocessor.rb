9aedb2f93a08e093eaad1df4a06b66603dddb942
Jakub Jirutka <jakub@jirutka.cz>
2022-02-05 01:50:15 +0100

    Invoke Postprocessor only for html5 backend (Asciidoctor's built-in)

--- lib/asciidoctor/katex/postprocessor.rb
+++ lib/asciidoctor/katex/postprocessor.rb
@@ -7,12 +7,23 @@ module Asciidoctor::Katex
   # Asciidcotor's built-in HTML converter, from the converted document.
   class Postprocessor < Asciidoctor::Extensions::Postprocessor
 
+    # @param document [Asciidoctor::Document] the document to process.
     # @param output [String] the converted document.
-    # @return [String] a modified *output*.
-    def process(_, output)
+    # @return [String] a modified *output*`.
+    def process(document, output)
+      return output if skip? document
+
       output
         .sub(%r{<script type="text/x-mathjax-config">.*?</script>}m, '')  # FIXME: broken in JS
         .sub(%r{<script src=".*?/MathJax\.js\?config=TeX-MML-AM_HTMLorMML"></script>}, '')
+    end
+
+    protected
+
+    # @param document [Asciidoctor::Document] the document to process.
+    # @return [Boolean] whether to skip processing of the *document*.
+    def skip?(document)
+      document.backend != 'html5'
     end
   end
 end
